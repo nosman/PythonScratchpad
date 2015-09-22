dddc994ec37815046ff29d60ef917a8ece502055
stephanos <sgt29@cornell.edu>
2015-09-21 22:55:23 -0700

    Got rid of prints.

--- functional.py
+++ functional.py
@@ -4,7 +4,6 @@ def curry(func):
     
     num_args = len(inspect.getargspec(func).args)
     args_list = []
-    print num_args
     
     def helper(arg):
         args_list.append(arg)
@@ -16,14 +15,3 @@ def curry(func):
             return helper
         
     return helper
-    
-x = [1,2,3,4,5]
-
-def add(x, y, z):
-    return x + y + z
-
-curry_add = curry(add)
-curry1 = curry_add(1)
-curry2 = curry1(2)
-curry3 = curry2(3)
-print(curry3)
